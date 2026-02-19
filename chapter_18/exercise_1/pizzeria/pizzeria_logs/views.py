42c8ddb305464502b02a8b166921d7efb6aed85f
Abdullahi <abkid04@gmail.com>
2026-02-20 00:06:34 +0100

    third

--- chapter_18/exercise_1/pizzeria/pizzeria_logs/views.py
+++ chapter_18/exercise_1/pizzeria/pizzeria_logs/views.py
@@ -10,3 +10,9 @@ def pizzas(request):
     pizzas = Pizza.objects.order_by('name')
     context = {'pizzas': pizzas}
     return render(request, 'pizzeria_logs/pizzas.xhtml', context)
+
+def pizza(request,pizza_id):
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = pizza.topping_set.order_by('name')
+    context = {'pizza': pizza, 'toppings': toppings}
+    return render(request, 'pizzeria_logs/toppings.xhtml', context)
