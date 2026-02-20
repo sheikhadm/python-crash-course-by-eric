65237565b8fccd06e1738442ba5a769c622f21a2
Abdullahi <abkid04@gmail.com>
2026-02-20 23:26:22 +0100

    last fourth

--- chapter_18/exercise_2/Blog/Blog/settings.py
+++ chapter_18/exercise_2/Blog/Blog/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blogs',
-    
+    'accounts'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -117,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_REDIRECT_URL = 'blogs:index'
+LOGOUT_REDIRECT_URL = 'blogs:index'
+
--- chapter_18/exercise_2/Blog/accounts/views.py
+++ chapter_18/exercise_2/Blog/accounts/views.py
@@ -12,9 +12,7 @@ def register(request):
             new_user = form.save()
 # Log the user in and then redirect to home page.
             login(request, new_user)
-            return redirect('learning_logs:index')
+            return redirect('blogs:index')
 # Display a blank or invalid form.
     context = {'form': form}
     return render(request, 'registration/register.xhtml', context)
-
-# Create your views here.
